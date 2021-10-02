f4748e8e4391cfaf9baa7c1bf3fef86a8bc63de3
Faj5097 <fab.jan@t-online.de>
2021-10-02 23:26:00 +0200

    Test and code change btn color

--- color-button/src/App.js
+++ color-button/src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <button
-        style={{ backgroundColor: "red" }}
+        style={{ backgroundColor: buttonColor }}
         onClick={() => {
           setButtonColor(newButtonColor);
         }}
--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -11,5 +11,5 @@ test("button has correct initial color", () => {
   fireEvent.click(colorBtn);
 
   expect(colorBtn.textContent).toBe("Change to red!");
-  expect(colorBtn).toHaveStyle({ backgroundColor: "red" });
+  expect(colorBtn).toHaveStyle({ backgroundColor: "blue" });
 });
