d2b8a591d7c60f2d65b436f74ccd2dfdedc620b7
Faj5097 <fab.jan@t-online.de>
2021-10-12 13:13:37 +0200

    Update files from course git repo

--- sundaes-on-demand/src/pages/entry/ToppingOption.jsx
+++ sundaes-on-demand/src/pages/entry/ToppingOption.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Col from "react-bootstrap/Col";
 
 export default function ToppingOption({ name, imagePath }) {
--- sundaes-on-demand/src/pages/entry/test/Options.test.jsx
+++ sundaes-on-demand/src/pages/entry/test/Options.test.jsx
@@ -2,28 +2,31 @@ import { render, screen } from "@testing-library/react";
 
 import Options from "../Options";
 
-test("display image for each scoop option from server", async () => {
+test("displays image for each scoop option from server", async () => {
   render(<Options optionType="scoops" />);
 
-  //find images
+  // find images
   const scoopImages = await screen.findAllByRole("img", { name: /scoop$/i });
   expect(scoopImages).toHaveLength(2);
 
-  //confirm alt text of images
+  // confirm alt text of images
+  // @ts-ignore
   const altText = scoopImages.map((element) => element.alt);
   expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
 });
 
-test("display image for each topping option from server", async () => {
+test("Displays image for each toppings option from server", async () => {
+  // Mock Service Worker will return three toppings from server
   render(<Options optionType="toppings" />);
 
-  //find images
-  const scoopImages = await screen.findAllByRole("img", { name: /topping$/i });
-  expect(scoopImages).toHaveLength(3);
+  // find images, expect 3 based on what msw returns
+  const images = await screen.findAllByRole("img", { name: /topping$/i });
+  expect(images).toHaveLength(3);
 
-  //confirm alt text of images
-  const altText = scoopImages.map((element) => element.alt);
-  expect(altText).toEqual([
+  // check the actual alt text for the images
+  // @ts-ignore
+  const imageTitles = images.map((img) => img.alt);
+  expect(imageTitles).toEqual([
     "Cherries topping",
     "M&Ms topping",
     "Hot fudge topping"
